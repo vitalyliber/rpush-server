562952163fe7f85be982611a6676de3c64730741
Vitaly Liber <zenamax@gmail.com>
2020-11-22 15:11:46 +0300

    fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'home#index'
-  resources :apps, only: [:index, :create, :destroy]
+  resources :apps, only: [:index, :create, :destroy] do
+    collection do
+      post :change_apns
+    end
+  end
   resources :mobile_devices, only: [:create, :destroy]
   resources :push_notifications, only: [:create]
 end
