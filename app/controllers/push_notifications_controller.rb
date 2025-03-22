249c4246735d108f076712b2587751c21e6097b6
vitaly liber <zenamax@gmail.com>
2025-03-22 21:54:11 +0300

    Support topic

--- app/controllers/push_notifications_controller.rb
+++ app/controllers/push_notifications_controller.rb
@@ -12,6 +12,12 @@ class PushNotificationsController < ApplicationController
     if params.dig(:mobile_user, :external_key).present?
       mobile_user = MobileUser.find_or_create_by!(mobile_users_params)
       SendPushesToUserJob.perform_later(mobile_user.id, notification.to_json)
+    elsif message_params[:topic].present?
+      PushNotificationService.send_pushes(
+        tokens: nil,
+        **clean_notification(notification),
+        topic: message_params[:topic]
+      )
     else
       SendPushesToEveryoneFirebaseJob.perform_later(current_app.id, notification.to_json) if params[:device_type] === "android"
     end
@@ -28,5 +34,9 @@ class PushNotificationsController < ApplicationController
   def mobile_users_params
     params.require(:mobile_user).permit(:external_key)
       .try { |hash| hash.merge(mobile_access: current_app, environment: "production") }
+  end
+
+  def clean_notification(notification)
+    notification.slice(*[:tokens, :title, :message, :data, :data_notification, :content_available, :topic])
   end
 end
--- app/services/push_notification_service.rb
+++ app/services/push_notification_service.rb
@@ -1,6 +1,6 @@
 class PushNotificationService
-  def self.send_pushes(tokens: [], title: '', message: '', data: {}, data_notification: {}, content_available: false)
-    tokens.each_slice(500) do |tokens_slice|
+  def self.send_pushes(tokens: [], title: '', message: '', data: {}, data_notification: {}, content_available: false, topic: nil)
+    send = ->(tokens_slice) do
       notification = {
         tokens: tokens_slice,
         notification: {
@@ -20,16 +20,27 @@ class PushNotificationService
             },
           },
         },
-        data: data.transform_values(&:to_s)
-      }
+        data: data.transform_values(&:to_s),
+        topic: topic
+      }.compact
 
       response = `node ./fcm_send.js '#{notification.to_json}' '#{Rpush::Fcm::App.last.json_key}'`
 
+      p(response) if Rails.env.development?
+
+      topic_text = topic ? "to topic #{topic}" : ""
+
       if response.include?("sent successfully")
-        Rails.logger.info("Message sent successfully: #{tokens.count}")
+        Rails.logger.info("Message sent successfully: #{tokens&.count || topic_text}")
       else
-        Rails.logger.error("Sending error: #{tokens.count} #{response}")
+        Rails.logger.error("Sending error: #{tokens&.count || topic_text} #{response}")
       end
+    end
+
+    send.call(nil) if topic
+
+    tokens&.each_slice(500) do |tokens_slice|
+      send.call(tokens_slice)
     end
   end
 end
